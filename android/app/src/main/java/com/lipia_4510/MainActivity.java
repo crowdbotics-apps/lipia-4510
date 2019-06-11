59fbbdc6fd0a821192f7f43b9641e704f0c70464
Crowdbotics <team@crowdbotics.com>
2019-06-11 10:56:18 +0000

    initial commit

---
+++ android/app/src/main/java/com/lipia_4510/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lipia_4510;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "lipia_4510";
+    }
+}
